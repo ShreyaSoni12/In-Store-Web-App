55ac83588b1aa6871f4b46087544582230807266
Shruti Chaubey <shrutichaubey2501@gmail.com>
2022-06-16 18:31:48 +0530

    instore project and home app created and form data is stored in the database

---
+++ instore/home/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from home.models import CustInsert
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+def recognize(request):
+    return render(request,'home_page2.html')
+def Insertrecord(request):
+    context={'success':False}
+    if request.method=="POST":
+            saverecord=CustInsert()
+            saverecord.name=request.POST.get('name')
+            saverecord.address=request.POST.get('address')
+            saverecord.amount=request.POST.get('amount')
+            saverecord.item_type=request.POST.get('item_type')
+            saverecord.description=request.POST.get('description')
+            saverecord.save()
+            context={'success':True}
+            return render(request,'home_page3.html',context)
+    else:
+        return render(request,'home_page2.html')
